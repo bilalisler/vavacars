9f32a949d73b8445afe97e286218e914cbe4a833
bilal <bilal.isler@dinodream.com>
2022-06-04 18:11:17 +0300

    mail sending

--- index.js
+++ index.js
@@ -1,5 +1,17 @@
 const axios = require('axios');
 const fs = require('fs');
+const nodemailer = require('nodemailer');
+
+let transporter = nodemailer.createTransport({
+    // host: 'smtp.mailtrap.io',
+    // port: 2525,
+    service: 'hotmail',
+    auth: {
+        user: "crawlervava@outlook.com",
+        pass: "Test123456789-"
+    }
+})
+
 
 let $url = 'https://app-vava-dtc-search-tr-prod.azurewebsites.net/search'
 let $params = {
@@ -41,19 +53,19 @@ fs.readFile('./lastCars.json', (err, data) => {
         var notBookedCarIdList = notBookedCars.map(car => car.id);
         var oldCarIdList = notBookedCarIdList.concat(bookedCarIdList);
 
-        // fs.writeFile('./lastCars.json', JSON.stringify([]), {flag: 'w'}, err => {}); // sıfırla
+        fs.writeFile('./lastCars.json', JSON.stringify([]), {flag: 'w'}, err => {}); // sıfırla
 
         var carList = [];
         setTimeout(() => {
             getCarList($params, function (newCarList) {
                 for (const [index, newCar] of Object.entries(newCarList)) {
                     if (!oldCarIdList.includes(newCar.id)) {
-                        console.log('newCar:', newCar.id,createLink(newCar))
+                        sendEmail(createLink(newCar));
                     }
                 }
 
                 carList = carList.concat(newCarList)
-                // fs.writeFileSync('./lastCars.json', JSON.stringify(carList), {flag: 'w'});
+                fs.writeFileSync('./lastCars.json', JSON.stringify(carList), {flag: 'w'});
             });
         }, 3000)
     }
@@ -76,4 +88,19 @@ getCarList = ($params, cb) => {
         .catch(function (error) {
             console.log(error);
         });
+}
+
+
+const sendEmail = (carLink) => {
+    const options = {
+        from : "crawlervava@outlook.com",
+        to: "mercan_shark@windowslive.com",
+        subject: "New Car in VAVA",
+        text: "Please check url: " + carLink
+    }
+    transporter.sendMail(options, (error, info) =>{
+        if(error){
+            console.log(error,'Error')
+        }
+    })
 }
