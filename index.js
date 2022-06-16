274517df61227629752766fa26d30370363602c7
bilal <bilal.isler@dinodream.com>
2022-06-16 22:28:09 +0300

    cron package was added

--- index.js
+++ index.js
@@ -1,97 +1,87 @@
 const axios = require('axios');
 const fs = require('fs');
 const nodemailer = require('nodemailer');
+const cron = require('node-cron');
 
-let transporter = nodemailer.createTransport({
-    service: 'hotmail',
-    auth: {
-        user: "crawlervava@outlook.com",
-        pass: "Test123456789-"
+startCrawler = () => {
+    const createLink = (car) => {
+        return 'https://tr.vava.cars/buy/cars/' + car.make + '/' + car.model + '/' + car.id
     }
-})
 
-
-let $url = 'https://app-vava-dtc-search-tr-prod.azurewebsites.net/search'
-let $params = {
-    "pageNum": 1,
-    "pageSize": 20,
-    "filters": {
-        "priceTo": 850000,
-        "mileageTo": 40000,
-        "yearFrom": 2018,
-        "transmission": ["Otomatik"],
-        "fuelType": [],
-        "driveType": [],
-        "bodyType": ["Crossover", "SUV"],
-        "doorCount": [],
-        "seatingCapacity": [],
-        "carFeaturesCodes": [],
-        "color": [],
-        "locationCity": [],
-        "tags": [],
-        "hideBooked": false,
-        "anyBooked": false
-    }
-}
-
-
-var filePath = __dirname + '/lastCars.json';
-
-console.log('crawler started...', (new Date()).toLocaleTimeString());
-
-fs.readFile(filePath, (err, data) => {
-    if (err) {
-        console.error(err, 'file read error');
-    } else {
-        var oldCarList = JSON.parse(data);
-        var oldCarIdList = oldCarList.map(car => car.id);
-
-        var carStatusMap = {};
-        for (const [index, item] of Object.entries(oldCarList)) {
-            carStatusMap[item.id] = item.status;
+    let $params = {
+        "pageNum": 1,
+        "pageSize": 20,
+        "filters": {
+            "priceTo": 850000,
+            "mileageTo": 40000,
+            "yearFrom": 2018,
+            "transmission": ["Otomatik"],
+            "fuelType": [],
+            "driveType": [],
+            "bodyType": ["Crossover", "SUV"],
+            "doorCount": [],
+            "seatingCapacity": [],
+            "carFeaturesCodes": [],
+            "color": [],
+            "locationCity": [],
+            "tags": [],
+            "hideBooked": false,
+            "anyBooked": false
         }
-
-        fs.writeFile(filePath, JSON.stringify([]), {flag: 'w'}, (err) => {
-            if (err)
-                console.log(err, 'write file error1');
-        }); // sıfırla
-
-        var carList = [];
-        setTimeout(() => {
-            getCarList($params, function (newCarList) {
-                let getStatus = function (status) {
-                    return status == 2 ? 'rezerve' : 'yeni'
-                };
-                for (const [index, newCar] of Object.entries(newCarList)) {
-                    let $link = createLink(newCar);
-                    let $newStatus = newCar.status;
-                    let $id = newCar.id;
-
-                    if ($id in carStatusMap) {
-                        let $oldStatus = carStatusMap[$id];
-                        if($oldStatus !== $newStatus){
-                            console.log($link + ' - ' + getStatus($oldStatus) + "'den " + getStatus($newStatus) + "'a çekilmiştir");
-                        }
-                    }else if (!oldCarIdList.includes($id) && newCar.status == 1 ) {
-                        console.log($link + ' -  yeni');
-                    }
-                }
-
-                carList = carList.concat(newCarList)
-                fs.writeFileSync(filePath, JSON.stringify(carList), {flag: 'w'}, (err) => {
-                    if (err)
-                        console.log(err, 'write file error2');
-                });
-            });
-        }, 3000)
     }
-});
 
-const createLink = (car) => {
-    return 'https://tr.vava.cars/buy/cars/' + car.make + '/' + car.model + '/' + car.id
+    var filePath = __dirname + '/lastCars.json';
+
+    fs.readFile(filePath, (err, data) => {
+        if (err) {
+            console.error(err, 'file read error');
+        } else {
+            var oldCarList = JSON.parse(data);
+            var oldCarIdList = oldCarList.map(car => car.id);
+
+            var carStatusMap = {};
+            for (const [index, item] of Object.entries(oldCarList)) {
+                carStatusMap[item.id] = item.status;
+            }
+
+            fs.writeFile(filePath, JSON.stringify([]), {flag: 'w'}, (err) => {
+                if (err)
+                    console.log(err, 'write file error1');
+            }); // sıfırla
+
+            var carList = [];
+            setTimeout(() => {
+                getCarList($params, function (newCarList) {
+                    let getStatus = function (status) {
+                        return status == 2 ? 'rezerve' : 'yeni'
+                    };
+                    for (const [index, newCar] of Object.entries(newCarList)) {
+                        let $link = createLink(newCar);
+                        let $newStatus = newCar.status;
+                        let $id = newCar.id;
+
+                        if ($id in carStatusMap) {
+                            let $oldStatus = carStatusMap[$id];
+                            if($oldStatus !== $newStatus){
+                                console.log($link + ' - ' + getStatus($oldStatus) + "'den " + getStatus($newStatus) + "'a çekilmiştir");
+                            }
+                        }else if (!oldCarIdList.includes($id) && newCar.status == 1 ) {
+                            console.log($link + ' -  yeni');
+                        }
+                    }
+
+                    carList = carList.concat(newCarList)
+                    fs.writeFileSync(filePath, JSON.stringify(carList), {flag: 'w'}, (err) => {
+                        if (err)
+                            console.log(err, 'write file error2');
+                    });
+                });
+            }, 3000)
+        }
+    });
 }
-
 getCarList = ($params, cb) => {
+    let $url = 'https://app-vava-dtc-search-tr-prod.azurewebsites.net/search'
     axios.post($url, $params)
         .then(function (response) {
             let totalCount = response.data.totalCount;
@@ -106,8 +96,14 @@ getCarList = ($params, cb) => {
         });
 }
 
-
 const sendEmail = (carLink) => {
+    let transporter = nodemailer.createTransport({
+        service: 'hotmail',
+        auth: {
+            user: "crawlervava@outlook.com",
+            pass: "Test123456789-"
+        }
+    })
     const options = {
         from: "crawlervava@outlook.com",
         to: "mercan_shark@windowslive.com",
@@ -120,3 +116,10 @@ const sendEmail = (carLink) => {
     //     }
     // })
 }
+
+console.log('crawler started...', (new Date()).toLocaleTimeString());
+cron.schedule('* * * * *', function() {
+    console.log('crawling started...', (new Date()).toLocaleTimeString());
+    startCrawler()
+    console.log('crawling finished...', (new Date()).toLocaleTimeString());
+});
