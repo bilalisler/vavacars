a4608e155780272cf06111580929101e2379a4ee
bilal <bilal.isler@dinodream.com>
2022-06-17 23:57:44 +0300

    minor updates

--- chrome-extension/background.js
+++ chrome-extension/background.js
@@ -3,28 +3,48 @@
 var notifyListById = {}
 let second = 1000;
 let minute = second * 60;
+var interval;
 
-setInterval(function () {
-    fetch('http://localhost:3000/')           //api for the get request
-        .then(response => response.json())
-        .then(messageList => {
-            if (messageList.length > 0) {
-                for (const [index, message] of Object.entries(messageList)) {
-                    let notifyId = 'notify_' + index;
+chrome.alarms.onAlarm.addListener(async (alarm) => {
+    let minutePeriod = await chrome.storage.sync.get(['minutePeriod']);
+    let fromPrice = await chrome.storage.sync.get(['fromPrice']);
+    let toPrice = await chrome.storage.sync.get(['toPrice']);
+    let fromMilage = await chrome.storage.sync.get(['fromMilage']);
+    let toMilage = await chrome.storage.sync.get(['toMilage']);
+    let fromYear = await chrome.storage.sync.get(['fromYear']);
+    let toYear = await chrome.storage.sync.get(['toYear']);
 
-                    notifyListById[notifyId] = message.link;
+    minutePeriod = minutePeriod['minutePeriod'];
+    fromPrice = fromPrice['fromPrice'];
+    toPrice = toPrice['toPrice'];
+    fromMilage = fromMilage['fromMilage'];
+    toMilage = toMilage['toMilage'];
+    fromYear = fromYear['fromYear'];
+    toYear = toYear['toYear'];
 
-                    chrome.notifications.create(notifyId, {
-                        type: 'basic',
-                        iconUrl: 'images/icon48.png',
-                        title: message.title,
-                        message: message.link,
-                        priority: 0
-                    });
+    interval = setInterval(function () {
+        fetch('http://localhost:3000/')           //api for the get request
+            .then(response => response.json())
+            .then(messageList => {
+                if (messageList.length > 0) {
+                    for (const [index, message] of Object.entries(messageList)) {
+                        let notifyId = 'notify_' + index;
+
+                        notifyListById[notifyId] = message.link;
+
+                        chrome.notifications.create(notifyId, {
+                            type: 'basic',
+                            iconUrl: 'images/icon48.png',
+                            title: message.title,
+                            message: message.link,
+                            priority: 0
+                        });
+                    }
                 }
-            }
-        });
-}, 5 * minute);
+            });
+    }, 5 * minute);
+})
+
 
 chrome.notifications.onClicked.addListener((notifyId) => { // bildirim'e tıklandığında
     let carLink = notifyListById[notifyId];
--- chrome-extension/popup.js
+++ chrome-extension/popup.js
@@ -1,12 +1,20 @@
 'use strict';
 
-// function saveForm(event) {
-//     console.log('event:', event)
-//
-//     chrome.action.setBadgeText({text: 'ON'});
-//     chrome.alarms.create('testAlarm',{delayInMinutes: 0});
-//
-//     window.close();
-// }
-//
-// document.getElementById('saveForm').addEventListener('click', saveForm);
+function saveForm(event) {
+    console.log('event:', event)
+
+    chrome.action.setBadgeText({text: 'ON'});
+    chrome.alarms.create('testAlarm',{delayInMinutes: 1});
+
+    chrome.storage.sync.set({minutePeriod: document.getElementById('minutePeriod').value});
+    chrome.storage.sync.set({fromPrice: document.getElementById('fromPrice').value});
+    chrome.storage.sync.set({toPrice: document.getElementById('toPrice').value});
+    chrome.storage.sync.set({fromMilage: document.getElementById('fromMilage').value});
+    chrome.storage.sync.set({toMilage: document.getElementById('toMilage').value});
+    chrome.storage.sync.set({fromYear: document.getElementById('fromYear').value});
+    chrome.storage.sync.set({toYear: document.getElementById('toYear').value});
+
+   // window.close();
+}
+
+document.getElementById('saveForm').addEventListener('click', saveForm);
